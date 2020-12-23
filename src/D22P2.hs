cf80e3e0ca26eb73872ae28e34c51820f4c31b7e
Val Aleksenko <v.aleksenko@gmail.com>
2020-12-23 08:58:23 -0500

    D22P2

--- src/D22P2.hs
+++ src/D22P2.hs
@@ -16,7 +16,7 @@ play = fst . until finished round . newGame
         finished ((x, y), _) = null x || null y
         round ((xx@(x:xs), yy@(y:ys)), p)
             | (any ((==) (xx, yy)) p) = ((xx, []), [])
-            | (length xs >= x) && (length ys >= y) = if (null . fst $ play (xs, ys)) then ((xs, ys ++ [y, x]), (xx, yy):p) else ((xs ++ [x, y], ys), (xx, yy):p)
+            | (length xs >= x) && (length ys >= y) = if (null . fst $ play (take x xs, take y ys)) then ((xs, ys ++ [y, x]), (xx, yy):p) else ((xs ++ [x, y], ys), (xx, yy):p)
             | otherwise = if x > y then ((xs ++ [x, y], ys), (xx, yy):p) else ((xs, ys ++ [y, x]), (xx, yy):p)
 
 {-
